f7478868a6ce5ec86ebc319579ff23c6a755207d
Azharangga Kusuma <97172969+azharanggakusuma@users.noreply.github.com>
2022-08-28 17:56:26 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,32 @@
+<?php
+$daftarMobil = array(
+    array(
+        'mobil' => "Avanza",
+        "Harga" => 500000
+    ),
+    array(
+        'mobil' => "Pajero Sport",
+        "Harga" => 700000
+    ),
+    array(
+        'mobil' => "Almaz",
+        "Harga" => 680000
+    ),
+    array(
+        'mobil' => "Civic",
+        "Harga" => 650000
+    ),
+    array(
+        'mobil' => "Model X",
+        "Harga" => 780000
+    ),
+    array(
+        'mobil' => "Jazz",
+        "Harga" => 600000
+    ),
+    array(
+        'mobil' => "Ranger",
+        "Harga" => 710000
+    )
+)
+?>
---
+++ script/main.js
@@ -0,0 +1,3 @@
+function onClick(){
+    alert("Pemesanan Telah Berhasil Dilakukan. Terima Kasih!");
+}
